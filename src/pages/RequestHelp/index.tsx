baf7257ec97f8f87e8fa2c394dda237802b33e50
Maurice Yap <mauriceyap@hotmail.co.uk>
2020-03-28 21:09:59 +0000

    remove ability to hide request help form

--- src/pages/RequestHelp/index.tsx
+++ src/pages/RequestHelp/index.tsx
@@ -1,49 +1,19 @@
 import { Typography } from "antd";
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 import RequestHelpForm from "./components/RequestHelpForm";
 
 const { Title, Paragraph } = Typography;
 
-declare global {
-  interface Window {
-    enableRequestForm: () => void;
-    disableRequestForm: () => void;
-  }
-}
-
-const RequestHelp: FunctionComponent<{}> = () => {
-  const [isRequestHelpFormEnabled, setRequestHelpFormEnabled] = useState(true);
-  window.enableRequestForm = () => setRequestHelpFormEnabled(true);
-  window.disableRequestForm = () => setRequestHelpFormEnabled(false);
-  return isRequestHelpFormEnabled ? (
-    <>
-      <Title>Request help from a volunteer</Title>
-      <Paragraph>
-        Send us a request using this form if your local group has a need for a
-        volunteer to translate or interpret. We'll do our best to find you a
-        match!
-      </Paragraph>
-      <RequestHelpForm />
-    </>
-  ) : (
-    <>
-      <Title>Request help from a volunteer</Title>
-      <Paragraph strong>
-        Since we have only just started, we have not yet started taking requests
-        from local mutual aid groups for help from volunteers. We will start
-        taking requests before Saturday 28th March 2020, so please come back and
-        check later.
-      </Paragraph>
-      <Paragraph>
-        Please be aware that in order to protect the data of volunteers who have
-        signed up, we will require verification that you belong to part of a
-        local mutual aid group before matching you up. This could involve us
-        verifying your email address as one which is used for a mutual aid
-        group, or through a Facebook profile which administers a mutual aid
-        group on Facebook.
-      </Paragraph>
-    </>
-  );
-};
+const RequestHelp: FunctionComponent<{}> = () => (
+  <>
+    <Title>Request help from a volunteer</Title>
+    <Paragraph>
+      Send us a request using this form if your local group has a need for a
+      volunteer to translate or interpret. We'll do our best to find you a
+      match!
+    </Paragraph>
+    <RequestHelpForm />
+  </>
+);
 
 export default RequestHelp;
