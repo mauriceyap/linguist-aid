7cfb92235cf2074e71f21517ccf2483af802c487
Maurice Yap <mauriceyap@hotmail.co.uk>
2020-03-28 21:09:27 +0000

    change logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,12 @@
 import React, { FunctionComponent } from "react";
 
 import "./logo.scss";
+import logo200 from "./logo-200.png";
 
 const Logo: FunctionComponent<{}> = () => (
-  <div className="logo">Linguist Aid</div>
+  <div className="logo">
+    <img src={logo200} alt="logo" />
+  </div>
 );
 
 export default Logo;
