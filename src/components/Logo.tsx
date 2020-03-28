010629a5cd40669159a6f6593023abd7282b8fc9
Maurice Yap <mauriceyap@hotmail.co.uk>
2020-03-28 13:42:23 +0000

    rebrand

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 
-import "./logo.css";
+import "./logo.scss";
 
 const Logo: FunctionComponent<{}> = () => (
   <div className="logo">Linguist Aid</div>
